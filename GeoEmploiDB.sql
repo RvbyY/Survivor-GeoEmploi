51c561931c2224b41ce9936cc4c9ed5f9ec3c45f
RvbyY <bassirou.tall@epitech.eu>
2026-09-03 10:24:57 +0200

    update env file

--- GeoEmploiDB.sql
+++ GeoEmploiDB.sql
@@ -2,8 +2,26 @@ CREATE DATABASE mydb;
 
 \c mydb;
 
-CREATE TABLE users {
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     email TEXT NOT NULL
-};
+);
+
+CREATE TABLE offers (
+    id SERIAL PRIMARY KEY,
+    offer_name VARCHAR(255) NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    company_id INTEGER NOT NULL REFERENCES users(id),
+    salary DOUBLE PRECISION NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    date TIMESTAMP DEFAULT NOW(),
+    max_distance DOUBLE PRECISION
+);
+
+CREATE TABLE candidacy (
+    id SERIAL PRIMARY KEY,
+    offer_id INTEGER REFERENCES offers(id),
+    candidate_id INTEGER REFERENCES users(id)
+);
